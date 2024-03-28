fff1c86479690dfef3cfd5e78a98c11e9e48ed1e
Nguyễn Hồng Phi <nguyenphi17022@gmail.com>
2024-03-29 02:49:55 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,38 @@
-import { createWebHistory, createRouter } from "vue-router";
+import {
+    createWebHistory,
+    createRouter
+} from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-const routes = [
-{
-path: "/",
-name: "contactbook",
-component: ContactBook,
-},
+
+const routes = [{
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true,
+    },
+    {
+        path: '/add-contact',
+        name: 'contact.add',
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true,
+    },
 ];
+
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes,
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes,
 });
+
 export default router;
